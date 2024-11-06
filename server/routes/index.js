14ee377ef183efc1c0529cbbc09b2e6f2b877aff
saad-21434318 <21434318@student.curtin.edu.au>
2024-11-05 21:20:49 -0800

    Added STUDENT ID in index.js

--- server/routes/index.js
+++ server/routes/index.js
@@ -4,7 +4,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'My Student ID is 21434318' });
+  res.render('index', { title: 'My Student ID is 21434318.' });
 });
 
 export default router;
